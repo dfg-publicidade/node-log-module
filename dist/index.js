f4644436d6135bb2942af8651dd16b3ef4aa7cb9
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2020-12-15 16:23:20 -0300

    Alteração no arquivo tsconfig.json

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,43 +7,41 @@ const ipware_1 = __importDefault(require("ipware"));
 const url_1 = __importDefault(require("url"));
 /* Module */
 class Log {
-    static emit(app, req, collectionName, obj) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const collection = app.db.collection(collectionName);
-            const getIp = ipware_1.default().get_ip;
-            const log = {};
-            log.app = app.info;
-            if (req) {
-                log.request = req.id;
-                log.action = url_1.default.parse(req.url).pathname;
-                log.method = req.method;
-                log.ip = getIp(req).clientIp;
-                if (req.usuario) {
-                    log.user = {
-                        id: req.usuario.id,
-                        nome: req.usuario.nome
-                    };
-                }
-                if (req.sistema) {
-                    log.system = {
-                        id: req.sistema.id,
-                        nome: req.sistema.nome
-                    };
-                }
+    static async emit(app, req, collectionName, obj) {
+        const collection = app.db.collection(collectionName);
+        const getIp = ipware_1.default().get_ip;
+        const log = {};
+        log.app = app.info;
+        if (req) {
+            log.request = req.id;
+            log.action = url_1.default.parse(req.url).pathname;
+            log.method = req.method;
+            log.ip = getIp(req).clientIp;
+            if (req.user) {
+                log.user = {
+                    id: req.user.id,
+                    nome: req.user.name
+                };
             }
-            if (obj) {
-                log.content = this.removeInvalidKeys(obj);
+            if (req.system) {
+                log.system = {
+                    id: req.system.id,
+                    nome: req.system.name
+                };
             }
-            log.time = new Date();
-            try {
-                yield collection.insertOne(log);
-            }
-            catch (error) {
-                // eslint-disable-next-line no-console
-                console.error(error);
-            }
-            return Promise.resolve();
-        });
+        }
+        if (obj) {
+            log.content = this.removeInvalidKeys(obj);
+        }
+        log.time = new Date();
+        try {
+            await collection.insertOne(log);
+        }
+        catch (error) {
+            // eslint-disable-next-line no-console
+            console.error(error);
+        }
+        return Promise.resolve();
     }
     static removeInvalidKeys(obj) {
         obj = Object.assign({}, obj);
